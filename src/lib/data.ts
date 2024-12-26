60f553057efab272168f78c3dc53559e8619347c
Apoorv <apoorv.mit2021@gmail.com>
2024-12-26 22:37:44 +0530

    Added data.ts

---
+++ src/lib/data.ts
@@ -0,0 +1,27 @@
+export const steps = [
+    {
+        id: 1,
+        title: "Step 1",
+        description: "This slide is 100% editable. Adapt it to your needs and capture your audience's attention.",
+    },
+    {
+        id: 2,
+        title: "Step 2",
+        description: "This slide is 100% editable. Adapt it to your needs and capture your audience's attention.",
+    },
+    {
+        id: 3,
+        title: "Step 3",
+        description: "This slide is 100% editable. Adapt it to your needs and capture your audience's attention.",
+    },
+    {
+        id: 4,
+        title: "Step 4",
+        description: "This slide is 100% editable. Adapt it to your needs and capture your audience's attention.",
+    },
+    {
+        id: 5,
+        title: "Step 5",
+        description: "This slide is 100% editable. Adapt it to your needs and capture your audience's attention.",
+    },
+];
